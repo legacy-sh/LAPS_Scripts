1f14f65a6fa31cd5f4998a1fbf6fb4257c9eec45
Leggy <50837811+legacy-sh@users.noreply.github.com>
2020-06-11 08:47:46 -0400

    Add files via upload

---
+++ Get Password.ps1
@@ -0,0 +1,10 @@
+﻿$val = 0
+
+While($val -ne 1) 
+    {
+    $comp = Read-Host 'What is the computer name?'
+    $ans = Read-Host 'Is this the correct computer name?' $comp
+    If ($ans.ToLower() -eq 'yes')
+     {Get-AdmPwdPassword -ComputerName $comp;break}
+    else {Continue}
+    }
---
+++ Reset Password.ps1
@@ -0,0 +1,11 @@
+﻿
+$val = 0
+
+While($val -ne 1) 
+    {
+    $comp = Read-Host 'What is the computer name?'
+    $ans = Read-Host 'Is this the correct computer name?' $comp
+    If ($ans.ToLower() -eq 'yes')
+     {Reset-AdmPwdPassword -ComputerName $comp;echo "Password is rest";break}
+    else {Continue}
+    }
